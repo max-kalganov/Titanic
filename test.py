f4264dd37184ca15378c67bf402b88e4dee1900e
mr_neither <mr.neitherman@gmail.com>
2018-08-17 20:26:34 +0300

    добавил вывод в правильном формате(csv)

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
-import numpy
+import main as m
+import numpy as np
+import csv
+
+def createCsvOutput(personId,answer):
+    path = "output.csv"
+    data = np.row_stack((personId, answer))
+    data = data.transpose()
+    csv_writer(data, path)
 
 
-a=5
-b=5
-print(a+b)
+def csv_writer(data, path):
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+
+
+a = np.array([1, 2, 3, 4, 5, 6, 7])
+b = np.array([1, 0, 1, 0, 0, 0, 1])
+createCsvOutput(a, b)
