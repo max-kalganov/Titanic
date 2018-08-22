3eb95469a6b0ebbe15529766b8e71241ef1745b4
mr_neither <mr.neitherman@gmail.com>
2018-08-22 18:22:51 +0300

    create csv writer

--- test.py
+++ test.py
@@ -2,9 +2,14 @@ import LogReg_class as m
 import numpy as np
 import csv
 
-def createCsvOutput(personId,answer):
+
+def create_csv_output(person_id, answer):
     path = "output.csv"
-    data = np.row_stack((personId, answer))
+    person_id = person_id.astype(str)
+    answer = answer.astype(str)
+    person_id = np.insert(person_id, 0, "PassengerId")
+    answer = np.insert(answer,  0, "Survived")
+    data = np.row_stack((person_id, answer))
     data = data.transpose()
     csv_writer(data, path)
 
@@ -18,4 +23,4 @@ def csv_writer(data, path):
 
 a = np.array([1, 2, 3, 4, 5, 6, 7])
 b = np.array([1, 0, 1, 0, 0, 0, 1])
-createCsvOutput(a, b)
+create_csv_output(a, b)
